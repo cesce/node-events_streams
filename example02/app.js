5f3f0d05367b0397e89dbf19b23680cddb8609ed
Cristian <gules01@gmail.com>
2020-06-09 17:22:54 +0200

    Different options on event requests

--- example02/app.js
+++ example02/app.js
@@ -9,6 +9,23 @@ function callbackServer(req, res) {
   res.end();
 }
 
-http.createServer(callbackServer).listen(port, () => {
+// All in one when creating the server
+/* http.createServer(callbackServer).listen(port, () => {
   console.log(`App is running in port ${port}`);
+}); */
+
+
+// We can create the server and put listening, but add requests on demand
+var server = http.createServer().listen(port, () => {
+  console.log(`App is running in port ${port}`);
+});
+
+server.on('request', callbackServer);
+
+server.on('close', () => {
+  console.log('Server is closed ...');
+});
+
+server.on('error', (err) => {
+  console.log(err);
 });
